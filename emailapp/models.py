8ac3cbad3cca2751e228bbaf8cb795873c3ff255
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-02-13 19:10:03 +0530

    Uploaded

--- emailapp/models.py
+++ emailapp/models.py
@@ -1,11 +1,27 @@
+# from django.db import models
+
+# class ValidEmail(models.Model):
+#     email = models.EmailField(unique=True)
+#     submission_date = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.email
+
+
+
+
+
 from django.db import models
 
 class ValidEmail(models.Model):
     email = models.EmailField(unique=True)
-    submission_date = models.DateTimeField(auto_now_add=True)
+    submission_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.email
+
+
+
 
 
 
