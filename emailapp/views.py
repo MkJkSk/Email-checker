c063f2923ba32361adf07184943598b56ab3f368
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-02-13 12:33:26 +0530

    Uploaded

--- emailapp/views.py
+++ emailapp/views.py
@@ -44,37 +44,34 @@
 
 
 
-# from .models import ValidEmail
-# from django.core.validators import validate_email
-# from django.core.exceptions import ValidationError
-# from django.shortcuts import render
+from .models import ValidEmail
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.shortcuts import render
 
-# def email_check(request):
-#     valid_emails = []
-#     invalid_emails = []
+def email_check(request):
+    valid_emails = []
+    invalid_emails = []
 
-#     if request.method == 'POST':
-#         emails = request.POST.get('emails', '').split()
+    if request.method == 'POST':
+        emails = request.POST.get('emails', '').split()
 
-#         for email in emails:
-#             try:
-#                 validate_email(email.strip())
-#                 valid_emails.append(email)
+        for email in emails:
+            try:
+                validate_email(email.strip())
+                valid_emails.append(email)
 
                 
-#                 existing_entry = ValidEmail.objects.filter(email=email.strip()).first()
+                existing_entry = ValidEmail.objects.filter(email=email.strip()).first()
 
-#                 if not existing_entry:
+                if not existing_entry:
                     
-#                     ValidEmail.objects.create(email=email.strip())
-#             except ValidationError:
-#                 invalid_emails.append(email)
+                    ValidEmail.objects.create(email=email.strip())
+            except ValidationError:
+                invalid_emails.append(email)
 
-#     return render(request, 'emailapp/email_check.html', {'valid_emails': valid_emails, 'invalid_emails': invalid_emails})
+    return render(request, 'emailapp/email_check.html', {'valid_emails': valid_emails, 'invalid_emails': invalid_emails})
 
 
-import MySQLdb
-
-print("MySQL client version:", MySQLdb.get_client_info())
 
 
