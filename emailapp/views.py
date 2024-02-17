85263e2bb3d74190839d1a33d42c5dc219a73cb7
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-02-17 16:45:57 +0530

    Feb_17

--- emailapp/views.py
+++ emailapp/views.py
@@ -20,30 +20,10 @@
 #     return render(request, 'emailapp/email_check.html', {'valid_emails': valid_emails, 'invalid_emails': invalid_emails})
 
 
-# from django.core.validators import validate_email                            {#PHASE 2}
-# from django.core.exceptions import ValidationError
-# from django.shortcuts import render
-
-# def email_check(request):
-#     valid_emails = []
-#     invalid_emails = []
-
-#     if request.method == 'POST':
-        
-#         emails = request.POST.get('emails', '').split()
-
-#         for email in emails:
-#             try:
-#                 validate_email(email.strip()) 
-#                 valid_emails.append(email)
-#             except ValidationError:
-#                 invalid_emails.append(email)
-
-#     return render(request, 'emailapp/email_check.html', {'valid_emails': valid_emails, 'invalid_emails': invalid_emails})
 
 
 
-
+                                                                    
 from .models import ValidEmail
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
