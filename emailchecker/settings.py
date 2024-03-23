3f4ffa010dc09935c7db0c71129a1dddb7e2e4f8
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-03-23 15:36:14 +0530

    Update the settings.py and wsgi.py files

--- emailchecker/settings.py
+++ emailchecker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3kd#c&8zmnrsb5zwn9+ing7e9w494x7u-wbfe5pl@987ws@rht
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
