d6d51e8e4a453111e9c879ad78dfd03ca8bab925
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-04-23 13:14:07 +0530

    updated

--- emailchecker/build_files.sh
+++ emailchecker/build_files.sh
@@ -1,10 +1,12 @@
 
 
-echo "BUILD START"
+eecho "BUILD START"
+
 python3.9 -m pip install -r requirements.txt
+
 python3.9 manage.py collectstatic --noinput --clear
 
-python manage.py migrate --database=default
-python manage.py migrate --database=mysql
+python3.9 manage.py migrate --database=default
+python3.9 manage.py migrate --database=mysql
 
 echo "BUILD END"
