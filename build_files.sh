7f13ef692d8587ff68a6328694c9a8c8316888b3
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-04-23 16:28:09 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,22 @@
+# echo "BUILD START"
+# python3.9 -m pip install -r requirements.txt 
+# python3.9 manage.py collectstatic --noinput --clear
+# echo "BUILD STOP"
+
+
+
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt 
-python3.9 manage.py collectstatic --noinput --clear
+
+# Activate the virtual environment
+source /path/to/your/virtualenv/bin/activate
+
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+# Run collectstatic command
+python manage.py collectstatic --noinput --clear
+
+# Deactivate the virtual environment
+deactivate
+
 echo "BUILD STOP"
