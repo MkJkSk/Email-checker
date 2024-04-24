6fc4449020b43969d99bfbef9081cd77f3e9e8f3
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-04-24 13:13:55 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,33 +1,17 @@
 echo "BUILD START"
 
-# Activate the virtual environment (assuming it's in the mk/Scripts directory)
-source ./emailchecker/mk/Scripts/activate.bat
+./emailchecker/mk/Scripts/activate.bat
 
-# Install dependencies from requirements.txt
+
 pip install -r requirements.txt
 
-# Run collectstatic command
+
 python manage.py collectstatic --noinput --clear
 
-# Deactivate the virtual environment
+
 deactivate
 
 echo "BUILD STOP"
 
 
-# echo "BUILD START"
-
-# # Activate the virtual environment
-# source mk/Scripts/activate
-
-# # Install dependencies from requirements.txt
-# mk/Scripts/pip install -r requirements.txt
-
-# # Run collectstatic command
-# mk/Scripts/python manage.py collectstatic --noinput --clear
-
-# # Deactivate the virtual environment
-# deactivate
-
-# echo "BUILD STOP"
 
