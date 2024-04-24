c96e5cc587d9d61b0320e724617e00f6de672232
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-04-24 18:13:55 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,12 @@
 echo "BUILD START"
 
-#  \emailchecker\mk\Scripts\activate.bat
+ \emailchecker\mk\Scripts\activate
 
 
 python3.9 -m pip install -r requirements.txt 
 python3.9 manage.py collectstatic --noinput --clear
  
-#  deactivate
+  deactivate
 
 echo "BUILD STOP"
 
