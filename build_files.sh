85e56ebe5ea7b99ca06f0d312a616549b841f95e
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-04-24 13:02:55 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,12 @@
 echo "BUILD START"
+
+source mk/Scripts/activate
+
 python3.9 -m pip install -r requirements.txt 
 python3.9 manage.py collectstatic --noinput --clear
+ 
+ deactivate
+
 echo "BUILD STOP"
 
 
