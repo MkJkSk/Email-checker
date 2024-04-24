f79c66089305da8522b86e4642de126032271171
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-04-24 17:09:30 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,30 @@
-echo "BUILD START"
+# echo "BUILD START"
 
- \emailchecker\mk\Scripts\activate.bat
+#  \emailchecker\mk\Scripts\activate.bat
 
 
-python3.9 -m pip install -r requirements.txt 
-python3.9 manage.py collectstatic --noinput --clear
+# python3.9 -m pip install -r requirements.txt 
+# python3.9 manage.py collectstatic --noinput --clear
  
- deactivate
+#  deactivate
 
-echo "BUILD STOP"
+# echo "BUILD STOP"
 
 
 
+@echo off
+echo BUILD START
+
+rem Activate virtual environment
+call emailchecker\mk\Scripts\activate.bat
+
+rem Install requirements
+python -m pip install -r requirements.txt
+
+rem Collect static files
+python manage.py collectstatic --noinput --clear
+
+rem Deactivate virtual environment
+deactivate
+
+echo BUILD STOP
