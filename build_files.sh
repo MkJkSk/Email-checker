09f3dd7374e20223c12fcbc8e9f017f1c6a4a340
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-04-25 17:33:30 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
-@echo off
+
 echo BUILD START
 
-call C:\Users\Lenovo\OneDrive\Desktop\Local_Pr\emailchecker\mk\Scripts\activate.bat
+cd C:\Users\Lenovo\OneDrive\Desktop\Local_Pr\emailchecker\mk\Scripts\activate.bat
 
 python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
