04018d97dd2d3ac5900c2d38a0c852bb992fe225
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-04-25 17:27:01 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
-# source /vercel/path0/.venv/bin/activate 
-pip install django
+source /vercel/path0/.venv/bin/activate 
+
 python3.9 -m pip install -r requirements.txt 
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD STOP"
