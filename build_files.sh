44f2854515425dd5b7a21b848393a6f816740d05
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-04-25 19:42:15 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -15,7 +15,7 @@ cd C:\Users\Lenovo\OneDrive\Desktop\Local_Pr\emailchecker\mk
 
 .\Scripts\activate.bat
 
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+python3.9.0 -m pip install -r requirements.txt
+python3.9.0 manage.py collectstatic --noinput --clear
 
 echo BUILD STOP
