9846964a1568e95d0a74fe8624b3b404911ecf37
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-04-25 19:47:38 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -9,11 +9,17 @@
 # echo BUILD STOP
 
 
+@echo off
 echo BUILD START
 
-cd C:\Users\Lenovo\OneDrive\Desktop\Local_Pr\emailchecker\mk
+cd /D "C:\Users\Lenovo\OneDrive\Desktop\Local_Pr\emailchecker\mk"
 
-.\Scripts\activate.bat
+if exist "Scripts\activate.bat" (
+    call Scripts\activate.bat
+) else (
+    echo Unable to find the virtual environment activation script.
+    exit /b 1
+)
 
 python3.9.0 -m pip install -r requirements.txt
 python3.9.0 manage.py collectstatic --noinput --clear
