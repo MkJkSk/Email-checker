3b367f282b5022f6cc86998035689c04c0419c80
Muzammil Ahmed khan <ahmedkhan.m@knowledgesprint.com>
2024-04-29 17:27:04 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -13,7 +13,7 @@
 echo BUILD START
 
 # Activate the virtual environment
-'mk/Scripts/activate'  # for Windows, use `mk\Scripts\activate`
+'mk/Scripts/activate/python.exe'  # for Windows, use `mk\Scripts\activate`
 
 # Install requirements
 python3.9 -m pip install -r requirements.txt
